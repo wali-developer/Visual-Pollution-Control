28128ccfe1cdff755060a7f8eb4b9ecd6183277c
Wali  Ullah <waliullah0540@gmail.com>
2023-06-02 01:07:25 +0500

    updates

--- src/views/auth/SignIn.jsx
+++ src/views/auth/SignIn.jsx
@@ -81,10 +81,10 @@ export default function SignIn() {
           <Link to="/admin">
             <button
               className="linear mt-2 w-full rounded-xl bg-brand-500 py-[12px] text-base font-medium text-white transition duration-200 hover:bg-brand-600 active:bg-brand-700 dark:bg-brand-400 dark:text-white dark:hover:bg-brand-300 dark:active:bg-brand-200"
-              onClick={(e) => {
-                e.preventDefault();
-                handleLogin();
-              }}
+              // onClick={(e) => {
+              //   e.preventDefault();
+              //   handleLogin();
+              // }}
             >
               Sign In
             </button>
